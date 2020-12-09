ab3486b1f8233ffd3cd589e7b3fe1cb35f8ef5bd
Peter Ringering <peteman316@gmail.com>
2020-12-08 21:11:25 -0700

    Working.

--- RingSoft.HomeLogix/Budget/BudgetExpenseWindow.xaml.cs
+++ RingSoft.HomeLogix/Budget/BudgetExpenseWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
+﻿using System;
+using System.Windows;
+using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
 using RingSoft.HomeLogix.DataAccess.Model;
 using RingSoft.HomeLogix.Library.ViewModels.Budget;
 
@@ -18,7 +20,41 @@ namespace RingSoft.HomeLogix.Budget
 
         public void SetViewType(RecurringViewTypes viewType)
         {
-            
+            EscrowLabel.Visibility = Visibility.Hidden;
+            EscrowBankAccount.Visibility = Visibility.Hidden;
+            EscrowStackPanel.Visibility = Visibility.Collapsed;
+
+            SpendingDayOfWeekLabel.Visibility = Visibility.Hidden;
+            SpendingDayOfWeekComboBoxControl.Visibility = Visibility.Hidden;
+            SpendingTypeStackPanel.Visibility = Visibility.Collapsed;
+
+            switch (viewType)
+            {
+                case RecurringViewTypes.Escrow:
+                    EscrowStackPanel.Visibility = Visibility.Visible;
+                    if (EscrowStackPanel.Visibility == Visibility.Visible)
+                    {
+                        var escrowBankAccountVisibility = Visibility.Hidden;
+                        if (ViewModel.DoEscrow != null && (bool) ViewModel.DoEscrow)
+                            escrowBankAccountVisibility = Visibility.Visible;
+
+                        EscrowLabel.Visibility = escrowBankAccountVisibility;
+                        EscrowBankAccount.Visibility = escrowBankAccountVisibility;
+                    }
+                    break;
+                case RecurringViewTypes.DayOrWeek:
+                    break;
+                case RecurringViewTypes.MonthlySpendingMonthly:
+                    SpendingTypeStackPanel.Visibility = Visibility.Visible;
+                    break;
+                case RecurringViewTypes.MonthlySpendingWeekly:
+                    SpendingTypeStackPanel.Visibility = Visibility.Visible;
+                    SpendingDayOfWeekLabel.Visibility = Visibility.Visible;
+                    SpendingDayOfWeekComboBoxControl.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(viewType), viewType, null);
+            }
         }
 
         public void OnValidationFail(FieldDefinition failedFieldDefinition)
@@ -28,6 +64,8 @@ namespace RingSoft.HomeLogix.Budget
 
         public void CloseWindow()
         {
+            DialogResult = ViewModel.DialogResult;
+
             Close();
         }
     }
