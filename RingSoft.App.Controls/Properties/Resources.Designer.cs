2f6facb7d057bf9f355b747a5b822165119a74f7
Peter Ringering <peteman316@gmail.com>
2020-12-09 20:55:21 -0700

    Working.

--- RingSoft.App.Controls/Properties/Resources.Designer.cs
+++ RingSoft.App.Controls/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace RingSoft.App.Controls.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
+        internal static byte[] Left {
+            get {
+                object obj = ResourceManager.GetObject("Left", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
         internal static byte[] RingSoft_Logo {
             get {
                 object obj = ResourceManager.GetObject("RingSoft_Logo", resourceCulture);
