bf24afae4a263428e48a11c6ab9b2b63cf7bd9eb
Peter Ringering <peteman316@gmail.com>
2022-09-16 23:21:28 -0600

    Working

--- RingSoft.HomeLogix.Sqlite/Migrations/20220917020118_AdFindDisabled.Designer.cs
+++ RingSoft.HomeLogix.Sqlite/Migrations/20220917020118_AdFindDisabled.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RingSoft.HomeLogix.Sqlite;
 
 namespace RingSoft.HomeLogix.Sqlite.Migrations
 {
     [DbContext(typeof(HomeLogixDbContext))]
-    partial class HomeLogixDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220917020118_AdFindDisabled")]
+    partial class AdFindDisabled
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RingSoft.HomeLogix.Sqlite/Migrations/20220917020118_AdFindDisabled.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RingSoft.HomeLogix.Sqlite.Migrations
+{
+    public partial class AdFindDisabled : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Disabled",
+                table: "AdvancedFinds",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disabled",
+                table: "AdvancedFinds");
+        }
+    }
+}
