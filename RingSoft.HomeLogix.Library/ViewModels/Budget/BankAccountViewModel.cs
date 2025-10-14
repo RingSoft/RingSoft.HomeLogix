945bc445d85a84230326767174c89445e3ed43e0
Peter Ringering <peteman316@gmail.com>
2025-10-14 15:08:13 -0600

    Working

--- RingSoft.HomeLogix.Library/ViewModels/Budget/BankAccountViewModel.cs
+++ RingSoft.HomeLogix.Library/ViewModels/Budget/BankAccountViewModel.cs
@@ -727,7 +727,7 @@ namespace RingSoft.HomeLogix.Library.ViewModels.Budget
                 if (LastCompleteDate.GetValueOrDefault().Year == 1980 && RegisterGridManager.Rows.Any())
                 {
                     var message =
-                        "Click Import Bank Transactions to input actual amounts from your bank statement and/or import a .QIF file that you download from your bank's web site.  This feature makes it quick and easy to complete Register Items and set actual amounts to Register Items and update the Current Balance.";
+                        "Click Import Bank Transactions to input actual amounts from your bank statement and/or import a .QIF file that you download from your bank's web site.  This feature makes it quick and easy to complete Register Items and set actual amounts to Register Items and update the Bank Balance.";
 
                     var caption = "Import Bank Transactions";
                     ControlsGlobals.UserInterface.ShowMessageBox(message, caption, RsMessageBoxIcons.Information);
