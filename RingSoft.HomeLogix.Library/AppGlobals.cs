32f0b0f1f37f8ba243ffc6b69e2da40c5c8bad84
Peter Ringering <peteman316@gmail.com>
2025-11-20 15:55:24 -0700

    Working

--- RingSoft.HomeLogix.Library/AppGlobals.cs
+++ RingSoft.HomeLogix.Library/AppGlobals.cs
@@ -68,7 +68,7 @@ namespace RingSoft.HomeLogix.Library
             RingSoftAppGlobals.AppTitle = "HomeLogix";
             RingSoftAppGlobals.AppCopyright = $"©{DateTime.Today.Year} by Peter Ringering";
             RingSoftAppGlobals.AppGuid = "7b811d4b-e39a-4316-a1dd-ba58eeafede7";
-            RingSoftAppGlobals.AppVersion = 414;
+            RingSoftAppGlobals.AppVersion = 415;
             RingSoftAppGlobals.PathToDownloadUpgrade = MasterDbContext.ProgramDataFolder;
             SystemGlobals.ProgramDataFolder = MasterDbContext.ProgramDataFolder;
         }
